6a82cccc9de1ca6564b028f59a3619bdacdf3e6c
Anders Oscarsen <andersoscarsen@gmail.com>
2014-11-13 00:56:33 +0100

    Full rewrite of to_mp3.sh
    Less buggy, just as awful

--- to_mp3.sh
+++ to_mp3.sh
@@ -1,26 +1,79 @@
 #!/bin/bash
 
-MP3DIR=/local/mp3 #TODO: make this an option
+MP3DIR=/home/anders/tmp/mp3 #TODO: make this an option
 
 for FLACFILE in "${@}"
 do
-    TAGS=$(metaflac --export-tags-to=- "$FLACFILE")
-    ARTIST=$(echo "$TAGS" | awk -F "=" 'toupper($1) ~ /^ARTIST$/{print $2;}')
-    ALBUM=$( echo "$TAGS" | awk -F "=" 'toupper($1) ~ /^ALBUM$/{ print $2;}')
-    TITLE=$( echo "$TAGS" | awk -F "=" 'toupper($1) ~ /^TITLE$/{ print $2;}')
-    TRACKNUMBER=$(echo "$TAGS" | awk -F "=" '
-        toupper($1) ~ /^TRACKNUMBER$/{
-            sub("/.*", "", $2);
-            print $2;}')
-    RELATIVETARGETDIR=$(echo "$ARTIST-$ALBUM" \
-        | sed   -e 's/[ \t]/_/g' \
-                -e 's/./\l&/g')
-    RELATIVETARGETFILE=$RELATIVETARGETDIR/$(echo "$TRACKNUMBER-$TITLE.mp3" \
-        | sed   -e 's/[ \t]/_/g' \
-                -e 's/./\l&/g')
-    TARGETDIR="$MP3DIR/$RELATIVETARGETDIR"
-    TARGETFILE="$MP3DIR/$RELATIVETARGETFILE"
-    mkdir -p "$TARGETDIR"
+    # csv the tags, pass non-ascii characters as variables
+    # I'm guessing most of this transliteration stuff, it's probably wrong
+    csv=$(metaflac --export-tags-to=- "$FLACFILE" | awk -F "=" '
+    {
+        gsub(/;/, "_", $2);
+        tag[toupper($1)] = $2;
+        # tolower("ABCÅÄÖ") -> "abcÅÄÖ", bug?
+
+        value = $2
+        # gsub(/[Öö]/, "o", "Öström") -> "oostroom", bug?
+        # swedish
+        gsub("Å", "A", value);
+        gsub("å", "a", value);
+        gsub("Ä", "A", value);
+        gsub("ä", "a", value);
+        gsub("Ö", "O", value);
+        gsub("ö", "o", value);
+        # icelandic
+        gsub("Á", "A", value);
+        gsub("á", "a", value);
+        gsub("Ð", "TH", value);
+        gsub("ð", "th", value);
+        gsub("É", "E", value);
+        gsub("é", "e", value);
+        gsub("Í", "I", value);
+        gsub("í", "i", value);
+        gsub("Ó", "O", value);
+        gsub("ó", "o", value);
+        gsub("Ú", "U", value);
+        gsub("ú", "u", value);
+        gsub("Ý", "Y", value);
+        gsub("ý", "y", value);
+        gsub("Æ", "AE", value);
+        gsub("æ", "ae", value);
+        gsub("Þ", "TH", value);
+        gsub("þ", "th", value);
+
+        value = tolower(value);
+        gsub(/[\/ ()]/, "_", value);
+
+        safe_tag[toupper($1)] = value;
+    }
+    END {
+    gsub(/\/.*/, "", safe_tag["TRACKNUMBER"]);
+    #     1 ;   2 ;      3 ;   4 ;     5 ;      6
+    # artist;album;tracknum;title;dirname;filename
+    printf("%s;%s;%.2i;%s;%s-%s;%.2i-%s.mp3\n",
+        tag["ARTIST"],
+        tag["ALBUM"],
+        safe_tag["TRACKNUMBER"],
+        tag["TITLE"],
+        safe_tag["ARTIST"],
+        safe_tag["ALBUM"],
+        safe_tag["TRACKNUMBER"],
+        safe_tag["TITLE"]);
+    }')
+
+    for idx in {1..6}
+    do
+        value=$(echo $csv | cut -d ";" -f $idx)
+        case $idx in
+            1) ARTIST=$value;;
+            2) ALBUM=$value;;
+            3) TRACKNUMBER=$value;;
+            4) TITLE=$value;;
+            5) FOLDERNAME=$value;;
+            6) FILENAME=$value;;
+        esac
+    done
+    mkdir -v "$MP3DIR/$FOLDERNAME"
     flac -cd "$FLACFILE" \
         | lame -V0 \
          --tt "$TITLE" \
@@ -28,5 +81,5 @@ do
          --ta "$ARTIST" \
          --tl "$ALBUM" \
          --add-id3v2 \
-         - "$TARGETFILE"
+         - "$MP3DIR/$FOLDERNAME/$FILENAME"
 done
